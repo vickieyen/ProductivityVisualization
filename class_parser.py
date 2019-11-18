5442223db6522dc49afa7b5e2ab833c5ea52449d
seiyoungahn <sedddong@gmail.com>
2019-11-17 22:24:45 -0800

    Add class parser for Java code

---
+++ class_parser.py
@@ -0,0 +1,107 @@
+import javalang
+
+class Class:
+    def __init__(self, file):
+        self.file = file
+        self.tree = javalang.parse.parse(file)
+        self.name = None
+        self.fields = []
+        self.methods = []
+        self.extends = None
+        self.implements = None
+        self.dependencies = []
+        
+        # parse class using tree provided
+        self.parse()
+
+    def parse(self):
+        self.parse_name(self.tree)
+        self.parse_implements(self.tree)
+        self.parse_extends(self.tree)
+        for body in self.tree.types[0].body:
+            if isinstance(body, javalang.tree.FieldDeclaration):
+                self.parse_field(body)
+            if isinstance(body, javalang.tree.MethodDeclaration):
+                self.parse_method(body)
+
+    def parse_name(self, tree):
+        self.name = tree.types[0].name.encode("utf-8")
+        print(self.name)
+
+    def parse_implements(self, tree):
+        implements = tree.types[0].implements
+        if (implements):
+            self.implements = implements.name.encode("utf-8")
+        print(self.implements)
+
+    def parse_extends(self, tree):
+        extends = tree.types[0].extends
+        if (extends):
+            self.extends = extends.name.encode("utf-8")
+        print(self.extends)
+    
+    def parse_field(self, field_dec):
+        field = Field()
+
+        # set access
+        field.access = "public"
+        if (field_dec.modifiers):
+            field.access = field_dec.modifiers.pop().encode("utf-8")
+
+        # set name
+        field.name = field_dec.declarators[0].name.encode("utf-8")
+
+        # set type
+        field.var_type = field_dec.type.name.encode("utf-8")
+
+        self.fields.append(field)
+        print(vars(field))
+
+    def parse_method(self, method_dec):
+        method = Method()
+
+        # set annotation
+        method.annotation = None
+        if (method_dec.annotations):
+            method.annotation = method_dec.annotations[0].name.encode("utf-8")
+
+        # set access
+        method.access = "public"
+        if (method_dec.modifiers):
+            method.access = method_dec.modifiers.pop().encode("utf-8")
+
+        # set name
+        method.name = method_dec.name.encode("utf-8")
+
+        # set parameters
+        method.params = []
+        for form_param in method_dec.parameters:
+            param = {"name": form_param.name.encode("utf-8"), "type": form_param.type.name.encode("utf-8")}
+            method.params.append(param)
+        
+        # set return type
+        method.return_type = "void"
+        if (method_dec.return_type):
+            method.return_type = method_dec.return_type
+
+        self.methods.append(method)
+        print(vars(method))
+
+    # feed names of the classes that you want to find dependencies for
+    # and it will return the class names that it depends on
+    def find_dependencies(self, class_names):
+        result = []
+        tokens = list(javalang.tokenizer.tokenize(self.file))
+        for token in tokens:
+            if (isinstance(token, javalang.tokenizer.Identifier)):
+                token_type = token.value.encode("utf-8")
+                if (token_type in class_names):
+                    result.append(token_type)
+        print(result)
+        return result
+
+class Field:
+    pass
+
+class Method:
+    pass
