fc69f2c370f2f8ed83c200b52c6246991d7f65d1
Vickie Yen <vickieyen@Vickies-MacBook-Pro.local>
2019-11-19 04:05:58 -0800

    get file source code from repo to generate plantuml

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ python 3.4+
 ### Install
 ```
 pip install PyGithub
-pip install gitpython
 pip install matplotlib
     Note: may need to isntall system requirements 
           brew install freetype
@@ -16,5 +15,4 @@ pip install javalang
 OR
 
 pip install -r requirements.txt
-** make sure you are using pip version 3 **
 ```
--- uml_diagram.py
+++ uml_diagram.py
@@ -1,14 +1,32 @@
 from github import Github
+import base64
 
 # DO NOT PUSH TOKEN, DELETE BEFORE PUSHING
-token = "dc594fcd6d293bb8004fc1c9ddf2a6f89d961ff8"
-g = Github(token)
+token = ""
 
+g = Github(token)
 repo = g.get_user().get_repo("ExamGenerator")
 
-commits = repo.get_commits()
+content_files = repo.get_contents("/src")
 
-files = []
 
-for commit in commits:
-    files.append(commit.files)
+def read_content_file(content_file):
+    content = content_file.content
+    decoded = base64.b64decode(content).decode("utf-8")
+    print(decoded)
+
+
+def traverse_content_files(content_files):
+    for content_file in content_files:
+        name = content_file.name.split('.')
+        if content_file.type == "dir":
+            traverse_content_files(repo.get_contents(content_file.path))
+        if len(name) < 2 or name[1] != "java":
+            continue
+        read_content_file(content_file)
+
+
+f = open("uml_output.txt","w+")
+f.write("@startuml")
+
+traverse_content_files(content_files)
